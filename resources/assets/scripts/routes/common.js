206513ef7e4635ab9bda6c6c91a48ab231c9b3fb
Irene Theiss <irene-rudel@gmx.at>
2020-08-24 20:07:43 +0200

    modify js

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,13 +1,21 @@
 export default {
   init() {
     // JavaScript to be fired on all pages
-    var eventSummary = $("article.event .entry-summary"),
+    var eventSummary = $("article .entry-summary"),
+        //blogMore = $('.btn.btn-details.blog-more'),
         btnDetail = $(".btn.btn-details");
 
     eventSummary.hide();
+    //blogMore.hide();
+
     btnDetail.on("click", function() {
       $(this).parent().parent().find(".entry-summary").slideToggle(500);
     });
+    
+   /*  blogMore.on("click", function() {
+      $(this).parent().parent().find(".entry-summary").slideToggle(500);
+    }); */
+
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
