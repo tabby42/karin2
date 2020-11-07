f5ef1a5fa1c046fdbc5bfde96b103742d11631c4
Irene Theiss <irene-rudel@gmx.at>
2020-11-07 10:24:51 +0100

    adapt markup for crowdfunding-button

--- resources/views/partials/frontpage-header.blade.php
+++ resources/views/partials/frontpage-header.blade.php
@@ -1,4 +1,8 @@
 <div id="fp-header" class="container-fluid">
+  <div class="crowdfunding-container">
+      <h3>das schöne neue Stück! <br>wünscht sich noch crowdfunding!</h3>
+      <a href="<?php echo get_site_url(null, '/?page_id=11'); ?>"  class="btn btn-primary btn-lg btn-details">mehr info</a>
+  </div>
   <h1>
     <span class="slogan-1">singend</span>
     <span class="slogan-2">springend</span>
